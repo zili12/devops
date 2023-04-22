10b7a8eb48958f7ff76827ef424dcbea397d097c
zili12 <ron1997ron@gmail.com>
2023-04-22 18:25:29 +0300

    Updated Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 	public static void main(String[]args){
 		for(int i=1;i<=10;i++){
-			System.out.println("Hello World..."+i);
+			System.out.println("Hello moshe..."+i);
 			}
 	}	
 }
