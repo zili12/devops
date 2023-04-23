c8e04b0a6ebf61bca6fde8eec8f9db14f3d76a28
Ron Zilberberg <93860114+zili12@users.noreply.github.com>
2023-04-23 17:35:30 +0300

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello{
 	public static void main(String[]args){
 		for(int i=1;i<=10;i++){
-			System.out.println("Hello moshe..."+i);
+			System.out.println("Hello ron..."+i);
 			}
 	}	
 }
